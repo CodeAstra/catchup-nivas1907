eb1d8e25b0a6a25f2e7997b882d7d453a69ac9aa
NIVAS <pasumarthynivas5@gmail.com>
2025-04-07 10:10:03 +0530

    Profile in post page clickable edit account section bug fixed

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -1,13 +1,14 @@
 class AccountController < ApplicationController
   def show
-    @posts=Post.where(user_id: current_user.id).order(created_at: :desc)
+    @user=User.find(params[:id])
+    @posts=Post.where(user_id: @user).order(created_at: :desc)
     @like=Like.all
   end
   def update
     @user=User.find(params[:id])
 
     if @user.update(permit)
-      redirect_to account_path
+      redirect_to account_path(params[:id])
     else
       redirect_to edit_account_path
     end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :like
+  has_many :like, dependent: :destroy
 end
