fac7a7476da5955ccb31c6b55839a44e58bd5498
NIVAS <pasumarthynivas5@gmail.com>
2025-04-08 11:23:32 +0530

    privacy feature

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -14,4 +14,9 @@ module AccountHelper
     val=Friend.exists?(user_id: current_user.id, friend_id: id, status: "accepted")||Friend.exists?(user_id: id, friend_id: current_user.id, status: "accepted")
     val
   end
+  def canishow(id)
+    ids=Friend.where(user_id: current_user.id , status: "accepted").pluck(:friend_id)+Friend.where(friend_id: current_user.id , status: "accepted").pluck(:user_id)
+    val=id==current_user.id||User.find(id).status=="public"||(User.find(id).status=="private" && Friend.exists?(user_id: current_user.id, friend_id: id, status: "accepted")||Friend.exists?(user_id: id, friend_id: current_user.id, status: "accepted"))||(User.find(id).status=="protected" && (Friend.exists?(user_id: ids, friend_id: id, status: "accepted")||Friend.exists?(user_id: id, friend_id: ids, status: "accepted")) ||Friend.exists?(user_id: current_user.id, friend_id: id, status: "accepted")||Friend.exists?(user_id: id, friend_id: current_user.id, status: "accepted")  )
+    val
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_07_071916) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_08_050004) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -76,6 +76,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_07_071916) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "bio"
+    t.string "status", default: "public"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
