47055c0713285c4ceff67d8b463c06f0e3fa562d
NIVAS <pasumarthynivas5@gmail.com>
2025-05-20 13:49:47 +0530

    final commit

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,24 +1,4 @@
 module AccountHelper
-  def pending_request(id)
-    current_user.received.pending.exists?(sender_id: id)
-  end
-
-  def getid(id, status)
-    if status=="received"
-      current_user.received.pending.where(sender_id: id).first
-    else
-      current_user.sent.rejected.where(reciver_id: id).first
-    end
-  end
-
-  def sent_request(id)
-    current_user.sent.pending.exists?(reciver_id: id)
-  end
-
-  def stranger(id)
-    current_user.accepted_friends_ids.exclude?(id)
-  end
-
   def canishow(id)
     (id==current_user.id)||User.find(id).public_state?||(User.find(id).private_state? && current_user.accepted_friends_ids.include?(id))||(User.find(id).protected_state? && (current_user.accepted_friends_ids.include?(id))|| (current_user.one_layer_friends_ids.include?(id)))
   end
--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,29 +1,2 @@
 module FriendshipsHelper
-  def sent_request(id)
-   current_user.sent.pending.exists?(reciver_id: id)
-  end
-
-  def checkf(id)
-    !current_user.accepted_friends_ids.include?(id)
-  end
-
-  def pending_request(id)
-    current_user.received.pending.exists?(sender_id: id)
-  end
-
-  def stranger(id)
-    current_user.accepted_friends_ids.exclude?(id)
-  end
-
-  def pending_list_count
-    current_user.pending_friends_ids.count
-  end
-
-  def getid(id, status)
-    if status=="received"
-      current_user.received.pending.where(sender_id: id).first
-    else
-      current_user.sent.rejected.where(reciver_id: id).first
-    end
-  end
 end
