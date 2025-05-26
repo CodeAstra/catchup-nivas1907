c7c5aa6649e7771066988e16b05d869a7ea0bc6a
NIVAS <pasumarthynivas5@gmail.com>
2025-05-26 11:59:29 +0530

    add post turbo bug fixed

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("click", function (e) {
+    if (e.target && e.target.classList.contains("cancel-button")) {
+      const formContainer = e.target.closest("turbo-frame");
+      if (formContainer) {
+        formContainer.remove(); 
+      }
+    }
+  });
+});
